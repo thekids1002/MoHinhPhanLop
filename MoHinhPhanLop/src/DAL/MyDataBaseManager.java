c4f0579e9e6bd43e55609a9c056e90afef3cf944
thekids1002 <phimtonghop7879@gmail.com>
2022-09-16 17:55:58 +0700

    init

---
+++ MoHinhPhanLop/src/DAL/MyDataBaseManager.java
@@ -0,0 +1,55 @@
+package DAL;
+
+import java.util.logging.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MyDataBaseManager {
+	private Connection c;
+	private Statement s;
+	private PreparedStatement p;
+	private String host, port, dbName, dbUser, dbPassword;
+
+	public MyDataBaseManager() {
+		super();
+		host = "localhost";
+		port = "3306";
+		dbUser = "root";
+		dbName = "school2";
+		dbPassword = "";
+	}
+
+	public void connectDB() {
+		String dbPath = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useUnicode=yes&characterEncoding=UTF-8";
+		try {
+			c = (Connection) DriverManager.getConnection(dbPath, dbUser, dbPassword);
+			s = c.createStatement();
+			System.out.print("Connected");
+		} catch (SQLException ex) {
+			System.out.print(ex.getMessage());
+		}
+	}
+
+	public ResultSet doReadQuery(String sql) {
+		ResultSet rs = null;
+		try {
+			rs = s.executeQuery(sql);
+		} catch (SQLException ex) {
+			Logger.getLogger(MyDataBaseManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return rs;
+	}
+
+//	public void doUpdateQuery() {
+//		try {
+//			p.executeUpdate();
+//		} catch (Exception ex) {
+//			Logger.getLogger(MyDataBaseManager.class.getName()).log(Level.SEVERE, null, ex);
+//		}
+//	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MoHinhPhanLop
+# QuanLyKhoaHoc
+Đồ án môn mô hình phân lớp
+B1 : Tạo cơ sở dữ liệu với tên là school2 và tiến hành import vào;
+<br>
+B2 : Dùng Eclipse mở project
