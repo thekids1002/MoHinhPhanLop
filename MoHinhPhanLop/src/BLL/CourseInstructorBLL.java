e6570e64617d226f94e338abe303341629b78f57
An Hữu <solock0983@gmail.com>
2022-09-24 11:34:03 +0700

    done grade and instructor BLL

--- MoHinhPhanLop/src/BLL/CourseInstructorBLL.java
+++ MoHinhPhanLop/src/BLL/CourseInstructorBLL.java
@@ -19,10 +19,18 @@ public class CourseInstructorBLL {
 			JOptionPane.showMessageDialog(null, "Mã khóa học không được để trống ");
 			return false;
 		}
+		if (instructor.getPersonID() < 0) {
+			JOptionPane.showMessageDialog(null, "Mã học viên không được để trống");
+			return false;
+		}
 		return new CourseInstructorDAL().addCourseInstructor(instructor);
 	}
 
 	public static boolean editCourseInstructor(CourseInstructor instructor) {
+		if (instructor.getCourseID() < 0) {
+			JOptionPane.showMessageDialog(null, "Mã khóa học không được để trống ");
+			return false;
+		}
 		if (instructor.getPersonID() < 0) {
 			JOptionPane.showMessageDialog(null, "Mã học viên không được để trống");
 			return false;
